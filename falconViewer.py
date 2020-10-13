78a653b94b019bbe8dbfe7631f6d80c079f3ee00
mas.ignacio@gmail.com <mas.ignacio@gmail.com>
2020-10-14 00:52:33 +0200

    Controls first

--- falconViewer.py
+++ falconViewer.py
@@ -2,44 +2,40 @@
 import cv2
 import imagezmq
 import numpy as np
+import json
 
-image_hub = imagezmq.ImageHub(open_port='tcp://rogueone:5555', REQ_REP=False)
-image_hub.connect('tcp://localhost:5555')
-bg=None
+image_hub = imagezmq.ImageHub(open_port='tcp://localhost:5555', REQ_REP=False)
 
-def nothing(x):
-    pass
 
-cv2.namedWindow('Trackbars')
-cv2.createTrackbar('L - H', 'Trackbars', 0, 179, nothing)
-cv2.createTrackbar('L - S', 'Trackbars', 0, 255, nothing)
-cv2.createTrackbar('L - V', 'Trackbars', 0, 255, nothing)
-cv2.createTrackbar('U - H', 'Trackbars', 179, 179, nothing)
-cv2.createTrackbar('U - S', 'Trackbars', 255, 255, nothing)
-cv2.createTrackbar('U - V', 'Trackbars', 255, 255, nothing)
+queue, image = image_hub.recv_image()
+msg=json.loads(queue)
+controls=msg['controls']
+print(controls)
+
+def cmd(x,key=None):
+    print(key,x)
+
+cv2.namedWindow('FalconViewer')
+on_trackbar={}
+for key,cn in controls.items():
+    if not cn['IsWritable']:
+        continue
+    print(key,  cn['MinValue'], cn['MaxValue'])
+    if cn['MaxValue']>=1000:
+        mult=1000
+    else:
+        mult=1
+    on_trackbar[key]=lambda x:cmd(x,key=f'{key}')
+    cv2.createTrackbar(f'{key}{ mult if mult!=1 else ""}', 'FalconViewer', int(cn['MinValue']/mult), int(cn['MaxValue']/mult), on_trackbar[key])
+
 
 while True:  # show streamed images until Ctrl-C
     queue, image = image_hub.recv_image()
-    if queue=='resizedJPG':
+    msg=json.loads(queue)
+    if msg['image_type']=='jpg':
         img=cv2.imdecode(image,1)
     else:
         img=image
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    cv2.imshow('gray', gray) # 1 window for each RPi
-    # if the background model is None, initialize it
-    if bg is None:
-       bg = np.zeros((img.shape[0], img.shape[1], 1), dtype = "float")
-       cv2.imshow('bg0', bg) # 1 window for each RPi
-    else:
-       bg= cv2.accumulateWeighted(gray, bg, .5)
-       print("P")
-    cv2.imshow('Trackbars', img) # 1 window for each RPi
-    cv2.imshow('bg', bg) # 1 window for each RPi
-    l_h = cv2.getTrackbarPos('L - H', 'Trackbars')
-    l_s = cv2.getTrackbarPos('L - S', 'Trackbars')
-    l_v = cv2.getTrackbarPos('L - V', 'Trackbars')
-    u_h = cv2.getTrackbarPos('U - H', 'Trackbars')
-    u_s = cv2.getTrackbarPos('U - S', 'Trackbars')
-    u_v = cv2.getTrackbarPos('U - V', 'Trackbars')
+    cv2.imshow('FalconViewer', img)
     cv2.waitKey(1)
 
--- falconZWOserver.py
+++ falconZWOserver.py
@@ -7,114 +7,118 @@ import time
 import datetime
 import zwoasi as asi
 import imagezmq
+import argparse
 import socket
 import cv2
+import json
 
 __author__ = 'Nacho Mas'
 __version__ = '0.1.0'
 __license__ = 'MIT'
 
 
-def save_control_values(filename, settings):
-    filename += '.txt'
-    with open(filename, 'w') as f:
-        for k in sorted(settings.keys()):
-            f.write('%s: %s\n' % (k, str(settings[k])))
-    print('Camera settings saved to %s' % filename)
+class ZWOcamera:
+    def __init__(self):
+        self.env_filename = os.getenv('ZWO_ASI_LIB')
+
+        if self.env_filename:
+            asi.init(self.env_filename)
+        else:
+            print('The filename of the SDK library is required \
+                (or set ZWO_ASI_LIB environment variable with the filename)')
+            sys.exit(1)
+
+        num_cameras = asi.get_num_cameras()
+        if num_cameras == 0:
+            print('No cameras found')
+            sys.exit(0)
+
+        cameras_found = asi.list_cameras()  # Models names of the connected cameras
+
+        if num_cameras == 1:
+            camera_id = 0
+            print('Found one camera: %s' % cameras_found[0])
+        else:
+            print('Found %d cameras' % num_cameras)
+            for n in range(num_cameras):
+                print('    %d: %s' % (n, cameras_found[n]))
+            # TO DO: allow user to select a camera
+            camera_id = 0
+            print('Using #%d: %s' % (camera_id, cameras_found[camera_id]))
+
+        self.camera = asi.Camera(camera_id)
+        self.camera_info = self.camera.get_camera_property()
+        self.controls = self.camera.get_controls()
+        self.init()
+        # Accept connections on all tcp addresses, port 5555
+        self.sender = imagezmq.ImageSender(connect_to='tcp://*:5555' ,REQ_REP=False)
+
+    def init(self):
+        self.camera.set_control_value(asi.ASI_HIGH_SPEED_MODE, 1)
+        self.camera.set_control_value(asi.ASI_BANDWIDTHOVERLOAD, self.controls['BandWidth']['MaxValue'])
+
+        # Set some sensible defaults. They will need adjusting depending upon
+        # the sensitivity, lens and lighting conditions used.
+        self.camera.disable_dark_subtract()
+        self.camera.set_control_value(asi.ASI_GAIN, 560)
+        self.camera.set_control_value(asi.ASI_EXPOSURE, 30000)
+        self.camera.set_control_value(asi.ASI_WB_B, 50)
+        self.camera.set_control_value(asi.ASI_WB_R, 50)
+        self.camera.set_control_value(asi.ASI_GAMMA, 40)
+        self.camera.set_control_value(asi.ASI_BRIGHTNESS,40)
+        self.camera.set_control_value(asi.ASI_FLIP, 0)
+        self.camera.set_image_type(asi.ASI_IMG_RGB24)
+  
+
+    def print_camera_controls(self):
+        # Get all of the camera controls
+        print('')
+        print('Camera controls:')
+        controls=self.controls
+        for cn in sorted(controls.keys()):
+            print('    %s:' % cn)
+            for k in sorted(controls[cn].keys()):
+                print('        %s: %s' % (k, repr(controls[cn][k])))
+        return controls
+
+    def run(self):
+        #camera.set_roi_format(800*1,400*1,1,asi.ASI_IMG_RGB24)
+        #camera.set_roi_start_position(0,0)
+        self.camera.start_video_capture()
+        img=self.camera.capture_video_frame()
+        #percent by which the image is resized
+        scale_percent = 20
+
+        width = int(img.shape[1] * scale_percent / 100)
+        height = int(img.shape[0] * scale_percent / 100)
+
+        # dsize
+        dsize = (width, height)
+        #dsize = (1600, 1000)
+
+        while True:
+            self.controls = self.camera.get_controls()
+            start=datetime.datetime.now()
+            img=self.camera.capture_video_frame()
+            end=datetime.datetime.now()
+            interval=end-start
+            values={}
+            values['times_start'] = start.strftime('%Y-%m-%d %H:%M:%S.%f')
+            values['times_end'] = end.strftime('%Y-%m-%d %H:%M:%S.%f')
+            values['times_interval'] = str(interval)
+            values['image_type'] = 'jpg'
+            values['camera_info']=self.camera_info
+            values['controls']=self.controls
+
+            resized=cv2.resize(img,dsize,cv2.INTER_NEAREST)
+            encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
+            result, encimg = cv2.imencode('.jpg', resized, encode_param)
+            self.sender.send_image(json.dumps(values), encimg)
+            #self.sender.send_image('resizedRAW', resized)
+            #self.sender.send_image('resizedJPG', encimg)
 
 
-env_filename = os.getenv('ZWO_ASI_LIB')
+if __name__ == '__main__':
 
-parser = argparse.ArgumentParser(description='Process and save images from a camera')
-parser.add_argument('filename',
-                    nargs='?',
-                    help='SDK library filename')
-args = parser.parse_args()
-
-# Initialize zwoasi with the name of the SDK library
-if args.filename:
-    asi.init(args.filename)
-elif env_filename:
-    asi.init(env_filename)
-else:
-    print('The filename of the SDK library is required (or set ZWO_ASI_LIB environment variable with the filename)')
-    sys.exit(1)
-
-num_cameras = asi.get_num_cameras()
-if num_cameras == 0:
-    print('No cameras found')
-    sys.exit(0)
-
-cameras_found = asi.list_cameras()  # Models names of the connected cameras
-
-if num_cameras == 1:
-    camera_id = 0
-    print('Found one camera: %s' % cameras_found[0])
-else:
-    print('Found %d cameras' % num_cameras)
-    for n in range(num_cameras):
-        print('    %d: %s' % (n, cameras_found[n]))
-    # TO DO: allow user to select a camera
-    camera_id = 0
-    print('Using #%d: %s' % (camera_id, cameras_found[camera_id]))
-
-camera = asi.Camera(camera_id)
-camera_info = camera.get_camera_property()
-
-# Get all of the camera controls
-print('')
-print('Camera controls:')
-controls = camera.get_controls()
-for cn in sorted(controls.keys()):
-    print('    %s:' % cn)
-    for k in sorted(controls[cn].keys()):
-        print('        %s: %s' % (k, repr(controls[cn][k])))
-
-
-# Use minimum USB bandwidth permitted
-#camera.set_control_value(asi.ASI_BANDWIDTHOVERLOAD, camera.get_controls()['BandWidth']['MinValue'])
-camera.set_control_value(asi.ASI_HIGH_SPEED_MODE, 1)
-camera.set_control_value(asi.ASI_BANDWIDTHOVERLOAD, camera.get_controls()['BandWidth']['MaxValue'])
-
-# Set some sensible defaults. They will need adjusting depending upon
-# the sensitivity, lens and lighting conditions used.
-camera.disable_dark_subtract()
-
-camera.set_control_value(asi.ASI_GAIN, 560)
-camera.set_control_value(asi.ASI_EXPOSURE, 300000)
-camera.set_control_value(asi.ASI_WB_B, 50)
-camera.set_control_value(asi.ASI_WB_R, 50)
-camera.set_control_value(asi.ASI_GAMMA, 40)
-camera.set_control_value(asi.ASI_BRIGHTNESS,40)
-
-# Accept connections on all tcp addresses, port 5555
-sender = imagezmq.ImageSender(connect_to='tcp://*:5555' ,REQ_REP=False) 
-camera.set_control_value(asi.ASI_FLIP, 0)
-
-camera.set_image_type(asi.ASI_IMG_RGB24)
-#camera.set_roi_format(800*1,400*1,1,asi.ASI_IMG_RGB24)
-#camera.set_roi_start_position(0,0)
-camera.start_video_capture()
-img=camera.capture_video_frame()
-#percent by which the image is resized
-scale_percent = 20
-
-width = int(img.shape[1] * scale_percent / 100)
-height = int(img.shape[0] * scale_percent / 100)
-
-# dsize
-dsize = (width, height)
-#dsize = (1600, 1000)
-
-while True:
-    img=camera.capture_video_frame()
-    timestamp = datetime.datetime.now()
-    cv2.putText(img, timestamp.strftime(
-          "%A %d %B %Y %I:%M:%S%p"), (10,  50),
-          cv2.FONT_HERSHEY_SIMPLEX, 1.35, (0, 0, 255), 1)
-    resized=cv2.resize(img,dsize,cv2.INTER_NEAREST)
-    encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
-    result, encimg = cv2.imencode('.jpg', resized, encode_param)
-    sender.send_image('resized', resized)
-    #sender.send_image('resizedRAW', resized)
-    #sender.send_image('resizedJPG', encimg)
+    camera=ZWOcamera()
+    camera.run()
