97980b95319d1876f9f9c2239c85ed84e01390c5
mas.ignacio@gmail.com <mas.ignacio@gmail.com>
2020-10-15 01:09:20 +0200

    Camera Controls ZMQ. WORKS OK!

--- falconViewer.py
+++ falconViewer.py
@@ -1,41 +1,73 @@
 # run this program on the Mac to display image streams from multiple RPis
 import cv2
 import imagezmq
+import zmq
 import numpy as np
 import json
+from functools import partial
+import logging
 
-image_hub = imagezmq.ImageHub(open_port='tcp://localhost:5555', REQ_REP=False)
+class ValWithCallback(object):
+    def __init__(self, val=0):
+        self.val = val
+    def change(self, val):
+        self.val = val
+
+class falconViewer:
+    def __init__(self):
+        logging.basicConfig(format='%(asctime)s %(levelname)s:falconViewer %(message)s',level=logging.DEBUG)
+        logging.info("Starting falcon Camara Viewer")
+        self.image_hub = imagezmq.ImageHub(open_port='tcp://localhost:5555', REQ_REP=False)
+        queue, image = self.image_hub.recv_image()
+        msg=json.loads(queue)
+        self.controls=msg['controls']
+        print(self.controls)
+        self.zmqcontext = zmq.Context()
+        self.CmdSocket = self.zmqcontext.socket(zmq.REQ)
+        self.CmdSocket.connect("tcp://localhost:5556")
+
+    def cb(self,key,x):
+        cn=self.controls[key]
+        if False:
+            if cn['MaxValue']>=1000:
+                mult=1000
+            else:
+                mult=1
+        else:
+            mult=1
+
+        ControlType=cn['ControlType']
+        d={ControlType:x*mult}   
+        logging.info(f'Sending {d}')
+        self.CmdSocket.send_string(json.dumps(d))
+        reply = self.CmdSocket.recv()
+        logging.info(reply)
+
+    def run(self):
+        cv2.namedWindow('FalconViewer')
+        for key,cn in self.controls.items():
+            if not cn['IsWritable']:
+                continue
+            print(key,  cn['MinValue'], cn['MaxValue'])
+            if cn['MaxValue']>=1000:
+                mult=1000
+            else:
+                mult=1
+            cv2.createTrackbar(f'{key}{ mult if mult!=1 else ""}', 'FalconViewer',\
+                 int(cn['MinValue']/mult), int(cn['MaxValue']/mult), partial(self.cb,key))
 
 
-queue, image = image_hub.recv_image()
-msg=json.loads(queue)
-controls=msg['controls']
-print(controls)
+        while True:  # show streamed images until Ctrl-C
+            queue, image = self.image_hub.recv_image()
+            msg=json.loads(queue)
+            logging.debug(f'times_interval:{msg["times_interval"]}')
+            if msg['image_type']=='jpg':
+                img=cv2.imdecode(image,1)
+            else:
+                img=image
+            cv2.imshow('FalconViewer', img)
+            cv2.waitKey(1)
 
-def cmd(x,key=None):
-    print(key,x)
-
-cv2.namedWindow('FalconViewer')
-on_trackbar={}
-for key,cn in controls.items():
-    if not cn['IsWritable']:
-        continue
-    print(key,  cn['MinValue'], cn['MaxValue'])
-    if cn['MaxValue']>=1000:
-        mult=1000
-    else:
-        mult=1
-    on_trackbar[key]=lambda x:cmd(x,key=f'{key}')
-    cv2.createTrackbar(f'{key}{ mult if mult!=1 else ""}', 'FalconViewer', int(cn['MinValue']/mult), int(cn['MaxValue']/mult), on_trackbar[key])
-
-
-while True:  # show streamed images until Ctrl-C
-    queue, image = image_hub.recv_image()
-    msg=json.loads(queue)
-    if msg['image_type']=='jpg':
-        img=cv2.imdecode(image,1)
-    else:
-        img=image
-    cv2.imshow('FalconViewer', img)
-    cv2.waitKey(1)
-
+if __name__ == '__main__':
+    viewer=falconViewer()
+    viewer.run()
--- falconZWOserver.py
+++ falconZWOserver.py
@@ -6,11 +6,23 @@ import sys
 import time
 import datetime
 import zwoasi as asi
+import zmq
 import imagezmq
 import argparse
 import socket
 import cv2
 import json
+import signal
+import logging
+import threading
+
+def threaded(fn):
+    def wrapper(*args, **kwargs):
+        t1 = threading.Thread(target=fn, args=args, kwargs=kwargs)
+        t1.start()
+        return t1
+
+    return wrapper
 
 __author__ = 'Nacho Mas'
 __version__ = '0.1.0'
@@ -19,32 +31,35 @@ __license__ = 'MIT'
 
 class ZWOcamera:
     def __init__(self):
+        logging.basicConfig(format='%(asctime)s %(levelname)s:falcon ZWOcamera %(message)s',level=logging.INFO)
+        logging.info("Starting falcon ZWO Camera Server")
+
         self.env_filename = os.getenv('ZWO_ASI_LIB')
 
         if self.env_filename:
             asi.init(self.env_filename)
         else:
-            print('The filename of the SDK library is required \
+            logging.error('The filename of the SDK library is required \
                 (or set ZWO_ASI_LIB environment variable with the filename)')
             sys.exit(1)
 
         num_cameras = asi.get_num_cameras()
         if num_cameras == 0:
-            print('No cameras found')
+            logging.error('No cameras found')
             sys.exit(0)
 
         cameras_found = asi.list_cameras()  # Models names of the connected cameras
 
         if num_cameras == 1:
             camera_id = 0
-            print('Found one camera: %s' % cameras_found[0])
+            logging.info('Found one camera: %s' % cameras_found[0])
         else:
-            print('Found %d cameras' % num_cameras)
+            logging.info('Found %d cameras' % num_cameras)
             for n in range(num_cameras):
-                print('    %d: %s' % (n, cameras_found[n]))
+                logging.info('    %d: %s' % (n, cameras_found[n]))
             # TO DO: allow user to select a camera
             camera_id = 0
-            print('Using #%d: %s' % (camera_id, cameras_found[camera_id]))
+            logging.info('Using #%d: %s' % (camera_id, cameras_found[camera_id]))
 
         self.camera = asi.Camera(camera_id)
         self.camera_info = self.camera.get_camera_property()
@@ -52,6 +67,13 @@ class ZWOcamera:
         self.init()
         # Accept connections on all tcp addresses, port 5555
         self.sender = imagezmq.ImageSender(connect_to='tcp://*:5555' ,REQ_REP=False)
+        self.zmqcontext = zmq.Context()
+        self.myCmdSocket = self.zmqcontext.socket(zmq.REP)        
+        self.myCmdSocket.bind("tcp://*:5556")
+        self.RUN = True
+        self.CMDThread = self.zmqQueue()
+        signal.signal(signal.SIGINT, self.signal_handler)
+
 
     def init(self):
         self.camera.set_control_value(asi.ASI_HIGH_SPEED_MODE, 1)
@@ -96,7 +118,7 @@ class ZWOcamera:
         dsize = (width, height)
         #dsize = (1600, 1000)
 
-        while True:
+        while self.RUN:
             self.controls = self.camera.get_controls()
             start=datetime.datetime.now()
             img=self.camera.capture_video_frame()
@@ -116,7 +138,58 @@ class ZWOcamera:
             self.sender.send_image(json.dumps(values), encimg)
             #self.sender.send_image('resizedRAW', resized)
             #self.sender.send_image('resizedJPG', encimg)
+    
+    def cmd(self,message):
+        msg=json.loads(message)
+        logging.info(msg)
+        cn=int(list(msg.keys())[0])
+        value=msg[list(msg.keys())[0]]
+        self.camera.set_control_value(cn,value)
+        return "OK"
 
+    @threaded
+    def zmqQueue(self):
+        '''this thread listen and process all the CMD from other nodes. **Run in background** '''
+        while self.RUN:
+            if True:
+                try:
+                        message = self.myCmdSocket.recv()
+                        logging.debug("RECV CMD: %s", message)
+                except:
+                        self.myCmdSocket.close()
+                        break
+
+                #  Do some 'work'
+                reply = self.cmd(message)
+                logging.debug("SEND CMD response: %s", reply)
+                #  Send reply back
+                self.myCmdSocket.send_string(str(reply))
+        logging.info("CMD LOOP END.")
+        return
+
+    def signal_handler(self, signal, frame):
+        '''Capture Ctril+C key'''
+        logging.info('You pressed Ctrl+C!')
+        self.end()
+        exit()
+
+    def __del__(self):
+        self.end()
+
+    def end(self):    
+        self.RUN = False
+        self.myCmdSocket.close()
+        logging.info("Term zmqcontext")
+        self.zmqcontext.term()
+        
+        logging.info("Waiting CMDthread end")
+        try:
+                self.CMDThread.join()
+        except:
+                logging.warn("Can't join CMD thread")
+
+        logging.info("***ENDED***")
+        return
 
 if __name__ == '__main__':
 
