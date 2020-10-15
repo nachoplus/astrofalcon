b19e0e88845ebcb4e7ef74021a57434058aae881
mas.ignacio@gmail.com <mas.ignacio@gmail.com>
2020-10-15 22:54:39 +0200

    Argumentos de linea de comandos

--- falconHTTP.py
+++ falconHTTP.py
@@ -7,20 +7,27 @@ from flask import render_template
 import threading
 import argparse
 import time
-
+import logging
+import json
 
 
 # initialize a flask object
 app = Flask(__name__)
 
+logging.basicConfig(format='%(asctime)s %(levelname)s:falconHTTP %(message)s',level=logging.DEBUG)
 def generate():
+    cameraServerIP=app.config.get('cameraServerIP')
     # grab global references to the output frame and lock variables
     # loop over frames from the output stream
-    image_hub = imagezmq.ImageHub(open_port='tcp://localhost:5555', REQ_REP=False)
-    image_hub.connect('tcp://localhost:5555')
+    image_hub = imagezmq.ImageHub(open_port=f'tcp://{cameraServerIP}:5555', REQ_REP=False)
     while True:
-        queue, outputFrame =image_hub.recv_image()
-        (flag, encodedImage) = cv2.imencode(".jpg", outputFrame)
+        queue, image =image_hub.recv_image()
+        msg=json.loads(queue)
+        if msg['image_type']=='jpg':
+            img=cv2.imdecode(image,1)
+        else:
+            img=image
+        (flag, encodedImage) = cv2.imencode(".jpg", img)
         # ensure the frame was successfully encoded
         # yield the output frame in the byte format
         yield(b'--frame\r\n' b'Content-Type: image/jpg\r\n\r\n' + 
@@ -44,13 +51,17 @@ def video_feed():
 if __name__ == '__main__':
     # construct the argument parser and parse command line arguments
     ap = argparse.ArgumentParser()
-    ap.add_argument("-i", "--ip", type=str, required=True,default='0.0.0.0',
+    ap.add_argument("-i", "--ip", type=str, default='0.0.0.0',
         help="ip address of the device")
-    ap.add_argument("-o", "--port", type=int, required=True,default=8088,
+    ap.add_argument("-o", "--port", type=int, default=8088,
         help="ephemeral port number of the server (1024 to 65535)")
+    ap.add_argument("-d", "--cameraServerIP", type=str, default='localhost',
+        help="ip address of Camera Server")
+
     args = vars(ap.parse_args())
-
-
+    logging.info(f"Connecting to:{args['cameraServerIP']}")
+    logging.info(f"HHTP Server on:{args['cameraServerIP']}:{args['port']}")
+    app.config['cameraServerIP'] =args['cameraServerIP']
     app.run(host=args["ip"], port=args["port"], debug=True,
         threaded=True, use_reloader=False)
 
--- falconViewer.py
+++ falconViewer.py
@@ -4,6 +4,7 @@ import imagezmq
 import zmq
 import numpy as np
 import json
+import argparse
 from functools import partial
 import logging
 
@@ -14,17 +15,17 @@ class ValWithCallback(object):
         self.val = val
 
 class falconViewer:
-    def __init__(self):
+    def __init__(self,cameraServerIP):
         logging.basicConfig(format='%(asctime)s %(levelname)s:falconViewer %(message)s',level=logging.DEBUG)
         logging.info("Starting falcon Camara Viewer")
-        self.image_hub = imagezmq.ImageHub(open_port='tcp://localhost:5555', REQ_REP=False)
+        self.image_hub = imagezmq.ImageHub(open_port=f'tcp://{cameraServerIP}:5555', REQ_REP=False)
         queue, image = self.image_hub.recv_image()
         msg=json.loads(queue)
         self.controls=msg['controls']
         print(self.controls)
         self.zmqcontext = zmq.Context()
         self.CmdSocket = self.zmqcontext.socket(zmq.REQ)
-        self.CmdSocket.connect("tcp://localhost:5556")
+        self.CmdSocket.connect(f'tcp://{cameraServerIP}:5556')
 
     def cb(self,key,x):
         cn=self.controls[key]
@@ -69,5 +70,14 @@ class falconViewer:
             cv2.waitKey(1)
 
 if __name__ == '__main__':
-    viewer=falconViewer()
+    # construct the argument parser and parse command line arguments
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-d", "--cameraServerIP", type=str, default='localhost',
+        help="ip address of Camera Server")
+
+    args = vars(ap.parse_args())
+    print(args['cameraServerIP'])
+    logging.info(f"Connecting to:{args['cameraServerIP']}...")
+    viewer=falconViewer(args['cameraServerIP'])
+    logging.info(f"{args['cameraServerIP']} CONNETED")
     viewer.run()
